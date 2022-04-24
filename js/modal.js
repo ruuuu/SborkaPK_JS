649dea9b823a046102451708242853c7c67ae55e
ruuuu <rufinka_91@mail.ru>
2022-04-24 16:24:33 +0300

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -2,11 +2,15 @@
 
 const modalBtn = document.querySelector('.modal__button'); // кнопка Оставить заявку
 const modal = document.querySelector('.modal'); // мод окно <form>
+// const closeBtn = document.querySelector('.close__button'); // кнопка  закрытия крестик
+const courseBtn = document.querySelector('.course__button'); // кнпока Заказать курс
+const closeBtn = document.createElement('button');
+
 
 
 modalBtn.addEventListener('click', () => {
-    modal.style.display = 'flex'; // display: flex
-    //console.dir(modal);
+    modal.style.display = 'flex';
+
 });
 
 
@@ -22,3 +26,35 @@ modal.addEventListener('click', (evt) => { // evt- объект события, 
 
 
 
+courseBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+});
+
+
+closeBtn.addEventListener('click', () => { // закртие мод окна, нажатие кнопки крестик
+    modal.style.display = 'none';
+});
+
+
+
+const createCloseBtn = () => {
+
+    const modalInner = document.querySelector('.modal__inner');
+    modalInner.style.position = 'relative'; // кнопку крестик будем позиционировать относительно этого элемента
+
+    // задаем стили крестику
+    closeBtn.style.cssText = `
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        width: 30px;
+        height: 30px;
+        background-image: url('../img/cansel.svg');
+        background-color: transparent;
+    `;
+
+    modalInner.append(closeBtn);
+}
+
+
+createCloseBtn();
