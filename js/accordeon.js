f2bf5db1d27fc570b7807fcfec20cc96c90731d8
ruuuu <rufinka_91@mail.ru>
2022-04-24 20:16:58 +0300

    Add files via upload

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,14 +1,36 @@
 const contents = document.querySelectorAll('.program-line__content'); // Nodelist [div,div,div,div]
+const constentDescrs = document.querySelectorAll('.program-line__descr');
 
 
 contents.forEach((elem) => { // на все элементы .program-line__content повесим обработчик события клика
 
     const title = elem.querySelector('.program-line__title');
-    const descr = elem.querySelector('.program-line__descr');
+    const descr = elem.querySelector('.program-line__descr'); // элемент на котрый нажали
     // console.dir(title.classList); // выведем элемент ввиде объекта чтоб увидеть какие свойства и методы у него есть
 
+    descr.style.transition = 'all .3s'; // плавно будет  отображать  текст блока
+    descr.style.overflow = 'hidden';
+
     title.addEventListener('click', () => {
-        descr.classList.toggle('active'); // classList- свойство элемента
+        //console.log('descr ', descr);
+
+        constentDescrs.forEach((tab, i) => {
+            // console.log(i, ' ая итерация');
+            // console.log('tab = ', tab);
+            // console.log(tab === descr);
+
+            if (tab === descr) { // descr - нажатый элемент(константа)
+                tab.classList.add('active');
+                console.dir(tab);
+                tab.style.height = tab.scrollHeight + 'px';
+
+            } else {
+                tab.classList.remove('active');
+                tab.style.height = 0;
+            }
+        })
+
+
     });
 
 
@@ -18,9 +40,19 @@ contents.forEach((elem) => { // на все элементы .program-line__cont
 
 
 
+
+
+
+
+
+
+
+
+
+
 //console.dir(document); // выведет элемент ввиде объекта
 
-// const elem = document.querySelector('.header');
+
 // const elems = document.querySelectorAll('div'); // псевдомассив(объект), NodeLIst
 
 
---
+++ js/timer.js
@@ -0,0 +1,15 @@
+// здесь пишем таймер котрый отсичтывает сколько дней осталась до опреденной даты
+const date = new Date();
+//console.log(date);
+console.dir(date);
+
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+
+console.dir(hours);
+console.dir(minutes);
+console.dir(seconds);
+
+
+// https://study.up-skills.ru/pl/teach/control/lesson/view?id=242386634&editMode=0 05Ж02
